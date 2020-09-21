1ea8c2b92de3b561a9c125d143b38d10d73821b4
Kunal Sinha <kunalsinhaiim@gmail.com>
2020-09-21 23:16:55 +0530

    env changes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask 
+import os
 app=Flask(__name__)
 
 @app.route('/')
 def version():
     return {'version':'0.001'}
 if __name__=='__main__':
-    app.run()
+    app.run(port=int(os.getenv('PORT')))
